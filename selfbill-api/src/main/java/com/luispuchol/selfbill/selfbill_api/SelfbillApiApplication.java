e0aa2aeef8c02382c24d4bc3107500464a649ba4
Luis Puchol <luisenriquepuchol@gmail.com>
2025-08-07 13:40:19 +0200

    Initial commit: SpringBoot backend structure

---
+++ selfbill-api/src/main/java/com/luispuchol/selfbill/selfbill_api/SelfbillApiApplication.java
@@ -0,0 +1,13 @@
+package com.luispuchol.selfbill.selfbill_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfbillApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfbillApiApplication.class, args);
+	}
+
+}
---
+++ selfbill-api/src/test/java/com/luispuchol/selfbill/selfbill_api/SelfbillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luispuchol.selfbill.selfbill_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfbillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
